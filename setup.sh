d243fc6353221d8768fd6bd6c412da3bef8ea21f
Benjamin Marwell <bmarwell@gmail.com>
2015-09-16 15:56:10 +0200

    Setup skript, subshells are not working...

--- setup.sh
+++ setup.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
+set -euo pipefail
 
-checkinstallmodule() {
-    local module=$1
+OLDPWD=$(pwd)
+cd DBD-JDBC
+cpanm -v --notest --installdeps .
+cd $OLDPWD
 
-    perl -M${module} -e '1' 2>/dev/null
-    perlmrc=$?
-
-    if [[ ${perlmrc} != 0 ]]; then
-        echo "Installing missing ${module} module."
-        perl -MCPAN -e "install ${module}"
-    fi
-}
-
-## Require XMLParser for XML::Simple
-checkinstallmodule XML::Parser
-
-## Require XML::SAX,XML::SAX::Expat,XML::NamespaceSupport for XML::Simple
-checkinstallmodule XML::SAX
-checkinstallmodule XML::SAX::Expat
-checkinstallmodule XML::NamespaceSupport
-
-## Require XML::Simple
-checkinstallmodule XML::Simple
-
-## Convert::BER
-checkinstallmodule Convert::BER
-
-# Obviously.
-checkinstallmodule DBI
